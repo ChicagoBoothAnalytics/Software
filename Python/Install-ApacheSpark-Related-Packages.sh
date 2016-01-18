bac6b023bf8d4fd8477d7c91325ac33f407609ce
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-01-18 03:10:59 -0600

    minor change

--- Python/Install-ApacheSpark-Related-Packages.sh
+++ Python/Install-ApacheSpark-Related-Packages.sh
@@ -3,4 +3,4 @@
 # this script requires an existing installation of Anaconda Python
 
 pip install --upgrade FindSpark
-pip install --upgrade Sparkit-Learn
+pip install --upgrade Sparkit-Learn --no-dependencies
