8ca5d51a2ec2e3a906bbd6bbd06dbf8c3e6ce0e7
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-01-18 02:27:41 -0600

    add script to install SQL-related Python packages

---
+++ Python/Install-SQL-Related-Packages.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# this script requires an existing installation of Anaconda Python
+
+
+# install / update SQLalchemy
+conda install SQLalchemy
+conda update sqlalchemy
+
+
+# install commonly-applicable drivers
+pip install --upgrade Egenix-mxODBC
+conda install PyODBC
+conda update pyodbc
+
+
+# install Firebird drivers
+pip install --upgrade FDB
+
+
+# install Microsoft SQL Server drivers
+pip install --upgrade PyMSSQL
+pip install --upgrade AdoDBAPI
+
+
+# download / install MySQL drivers
+# pip install --upgrade CyMySQL   SKIP: requires C++
+conda install MySQL-Connector-Python
+conda update mysql-connector-python
+conda install MySQL-Python
+conda update MySQL-Python
+# pip install --upgrade OurSQL   SKIP: requires Cython extension
+conda install PyMySQL
+conda update pymysql
+
+
+# download / install PostgreSQL drivers
+pip install --upgrade PG8000
+# pip install --upgrade Py-PostgreSQL   SKIP: for Python 3.1 and greater only
+conda install psycopg2   # for Windows, install from .whl here: http://www.lfd.uci.edu/~gohlke/pythonlibs/#psycopg
+conda update psycopg2
+# pip install --upgrade psycopg2cffi   SKIP: requires PostgreSQL installed
+
+
+# install SQLite drivers
+# pip install --upgrade PySQLCipher   SKIP: requires OpenSSL
+# pip install --upgrade PySQLite   SKIP: SQLite3 package already included in Python >=2.5 distributions
