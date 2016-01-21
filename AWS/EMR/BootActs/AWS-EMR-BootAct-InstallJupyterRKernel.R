ff4c0624637c7900aa42eb8992a4ddbff46af50a
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-01-20 23:25:53 -0600

    update Jupyter R Kernel installation script

--- AWS/EMR/BootActs/AWS-EMR-BootAct-InstallJupyterRKernel.R
+++ AWS/EMR/BootActs/AWS-EMR-BootAct-InstallJupyterRKernel.R
@@ -1,8 +1,6 @@
-library(devtools)
-install_github('armstrtw/rzmq')
-
-install.packages(c('repr', 'IRkernel', 'IRdisplay'),
-                 repos=c('http://irkernel.github.io',
-                         'http://cran.rstudio.com'))
+install.packages(
+  c('rzmq', 'repr', 'IRkernel', 'IRdisplay'),
+  repos=c('http://irkernel.github.io',
+          'http://cran.rstudio.com'))
 
 IRkernel::installspec()
