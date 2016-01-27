bf3e59c3623f5cc6c373306b7f964d68666a6b5b
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-01-27 12:50:21 -0600

    minor code re-shuffle

--- AWS/EMR/connect.sh
+++ AWS/EMR/connect.sh
@@ -5,10 +5,6 @@ PORT=8133
 MONITORING_PORT=4040
 
 
-# open Jupyter notebook on web browser
-python -m webbrowser http://localhost:$PORT
-
-
 # parse command-line options
 while getopts "d:" opt
 do
@@ -20,10 +16,12 @@ do
 done
 
 
-# connect to AWS EMR Cluster's iPython Notebook via a pipe
-echo "Please open LOCALHOST:$PORT on your web browser"
+echo "Opening LOCALHOST:$PORT on your web browser..."
+python -m webbrowser http://localhost:$PORT
+
+echo "Please enter 'yes' if asked 'Are you sure you want to continue connecting (yes/no)?' below; then please wait a few seconds."
+# connect to AWS EMR Cluster's Jupyter Notebook environment via a pipe
 ssh -o ServerAliveInterval=10 -i keypair.pem -N -L $PORT:$AWS_EMR_CLUSTER_PUBLIC_DNS:$PORT hadoop@$AWS_EMR_CLUSTER_PUBLIC_DNS
 
-
 # to monitor Spark cluster:
-# ssh -o ServerAliveInterval=10 -i keypair.pem -N -L 4040:<Cluster Master DNS>:4040 hadoop@<Cluster Master DNS>
+# ssh -o ServerAliveInterval=10 -i keypair.pem -N -L 4040:<Cluster Master Public DNS>:4040 hadoop@<Cluster Master Public DNS>
