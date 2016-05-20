ef6de8e6318d077b1a9e634fa5f7879ed2646620
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-05-19 23:04:32 -0500

    add installation of Python Django packages

---
+++ Python/Install-Django-Packages.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# this script requires an existing installation of Anaconda Python
+
+
+conda install Django
+conda update django
+
+pip install --upgrade Django-AppConf
+
+pip install --upgrade Django-AutoComplete-Light
+
+pip install --upgrade Django-Bootstrap-Markdown
+
+pip install --upgrade Django-Classy-Tags
+
+pip install --upgrade Django-DateTime-Widget
+
+pip install --upgrade Django-Easy-Maps
+
+pip install --upgrade Django-Extra-Views
+
+pip install --upgrade Django-Guardian
+
+pip install --upgrade Django-Haystack
+
+pip install --upgrade Django-ImageKit
+
+pip install --upgrade Django-Keyboard-Shortcuts
+
+pip install --upgrade Django-Userena
+
+pip install --upgrade Easy-Thumbnails
+
+pip install --upgrade ElasticSearch==1.6.0
+
+pip install --upgrade GeoPy
+
+pip install --upgrade HTML2Text
+
+pip install --upgrade PILkit
+
+conda install Pillow
+conda update pillow
+
+conda install PyTZ
+conda update pytz
+
+conda install Six
+conda update six
+
+pip install --upgrade URLlib3
+
+conda install Wheel
+conda update wheel
