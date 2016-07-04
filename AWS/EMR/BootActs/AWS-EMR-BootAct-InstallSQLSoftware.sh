e12dea7921335ea1e2a971739862c9ede65eed75
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-07-04 15:13:25 -0700

    update SQL packages for AWS EMR

--- AWS/EMR/BootActs/AWS-EMR-BootAct-InstallSQLSoftware.sh
+++ AWS/EMR/BootActs/AWS-EMR-BootAct-InstallSQLSoftware.sh
@@ -25,9 +25,8 @@ sudo yum install -y unixODBC-devel
 echo `df -h / | sed -n 2p` UnixODBC >> $MAIN_DISK_USAGE_LOG
 
 
-# install SQLalchemy
-sudo pip install --upgrade SQLalchemy
-echo `df -h / | sed -n 2p` SQLalchemy >> $MAIN_DISK_USAGE_LOG
+# install XlRd
+sudo pip install --upgrade XlRd
 
 
 # install CSVkit
@@ -35,8 +34,9 @@ sudo pip install --upgrade CSVkit
 echo `df -h / | sed -n 2p` CSVkit >> $MAIN_DISK_USAGE_LOG
 
 
-# install XlRd
-sudo pip install --upgrade XlRd
+# install SQLalchemy
+sudo pip install --upgrade SQLalchemy
+echo `df -h / | sed -n 2p` SQLalchemy >> $MAIN_DISK_USAGE_LOG
 
 
 # install commonly-applicable drivers
@@ -46,7 +46,7 @@ echo `df -h / | sed -n 2p` Egenix-mxODBC >> $MAIN_DISK_USAGE_LOG
 sudo pip install --upgrade PyODBC
 echo `df -h / | sed -n 2p` PyODBC >> $MAIN_DISK_USAGE_LOG
 
-# sudo pip install --upgrade zxJDBC   SKIPPED: for Jython only
+# sudo pip install --upgrade zxJDBC   # SKIPPED: for Jython only
 # echo `df -h / | sed -n 2p` zxJDBC >> $MAIN_DISK_USAGE_LOG
 
 
@@ -54,7 +54,7 @@ echo `df -h / | sed -n 2p` PyODBC >> $MAIN_DISK_USAGE_LOG
 sudo pip install --upgrade FDB
 echo `df -h / | sed -n 2p` Firebird DB >> $MAIN_DISK_USAGE_LOG
 
-# sudo pip install --upgrade kinterbasdb --allow-external kinterbasdb --allow-unverified kinterbasdb   SKIPPED: very old!
+# sudo pip install --upgrade kinterbasdb --allow-external kinterbasdb --allow-unverified kinterbasdb   # SKIPPED: very old!
 
 wget https://github.com/FirebirdSQL/jaybird/releases/download/v2.2.9/Jaybird-2.2.9-JDK_1.8.zip
 unzip Jaybird-* -d Jaybird-2.2.9
@@ -75,8 +75,7 @@ echo `df -h / | sed -n 2p` AdoDBAPI >> $MAIN_DISK_USAGE_LOG
 sudo yum groupinstall -y --setopt=group_package_types=mandatory,default,optional "MySQL Database"
 sudo yum groupinstall -y --setopt=group_package_types=mandatory,default,optional "MySQL Database Client"
 
-sudo yum install -y mysql
-sudo yum install -y mysql-devel
+sudo yum install -y mysql mysql-devel
 echo `df -h / | sed -n 2p` MySQL YUM Packages >> $MAIN_DISK_USAGE_LOG
 
 sudo pip install --upgrade CyMySQL
@@ -94,31 +93,30 @@ echo `df -h / | sed -n 2p` OurSQL >> $MAIN_DISK_USAGE_LOG
 sudo pip install --upgrade PyMySQL
 echo `df -h / | sed -n 2p` PyMySQL >> $MAIN_DISK_USAGE_LOG
 
-wget http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.38.zip
+wget http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.39.zip
 unzip mysql-connector-java-*
 sudo rm mysql-connector-java*.zip
-sudo mv mysql-connector-java*/mysql-connector-java*.jar MySQL-JDBC-5.1.38.jar
+sudo mv mysql-connector-java*/mysql-connector-java*.jar MySQL-JDBC-5.1.39.jar
 sudo rm -r mysql-connector-java*/
 
 
 # install Oracle drivers
-# sudo pip install --upgrade cx-Oracle   SKIPPED: requires Oracle software installed
+# sudo pip install --upgrade cx-Oracle   # SKIPPED: requires Oracle software installed
 
-wget http://download.oracle.com/otn/utilities_drivers/jdbc/11204/ojdbc6.jar -O Oracle-JDBC-6.jar
+wget http://download.oracle.com/otn/utilities_drivers/jdbc/121020/ojdbc7.jar -O Oracle-JDBC-7.jar
 
 
 # download / install PostgreSQL Python & JDBC drivers
 sudo yum groupinstall -y --setopt=group_package_types=mandatory,default,optional "PostgreSQL Database Server (Version 8)"
 sudo yum groupinstall -y --setopt=group_package_types=mandatory,default,optional "PostgreSQL Database Client (Version 8)"
 
-sudo yum install -y postgresql
-sudo yum install -y postgresql-devel
+sudo yum install -y postgresql postgresql-devel
 echo `df -h / | sed -n 2p` PostgreSQL YUM Packages >> $MAIN_DISK_USAGE_LOG
 
 sudo pip install --upgrade PG8000
 echo `df -h / | sed -n 2p` PG8000 >> $MAIN_DISK_USAGE_LOG
 
-# sudo pip install --upgrade Py-PostgreSQL   skip: for Python 3.1 and greater only
+# sudo pip install --upgrade Py-PostgreSQL   # SKIPPED: for Python 3.1 and greater only
 # echo `df -h / | sed -n 2p` Py-PostgreSQL >> $MAIN_DISK_USAGE_LOG
 
 sudo yum install -y python-psycopg2
@@ -132,15 +130,15 @@ curl https://jdbc.postgresql.org/download/postgresql-9.4.1207.jar --output Postg
 
 
 # install SQLite drivers
-# sudo pip install --upgrade PySQLCipher   SKIPPED: needs Cython? or somehow fails on GPU instances
+# sudo pip install --upgrade PySQLCipher   # SKIPPED: needs Cython? or somehow fails on GPU instances
 # echo `df -h / | sed -n 2p` PySQLCipher >> $MAIN_DISK_USAGE_LOG
-# sudo pip install --upgrade PySQLite   SKIPPED: SQLite3 package already included in Python >=2.5 distributions
+# sudo pip install --upgrade PySQLite   # SKIPPED: SQLite3 package already included in Python >=2.5 distributions
 
 git clone https://github.com/xerial/sqlite-jdbc
 cp sqlite-jdbc/lib/jdbc-api-1.4.jar SQLite-JDBC-1.4.jar
 
 # install Sybase drivers
-# sudo pip install --upgrade git+git://github.com/fbessho/python-sybase.git   skip: requires Sybase software installed
+# sudo pip install --upgrade git+git://github.com/fbessho/python-sybase.git   # SKIPPED: requires Sybase software installed
 
 
 # install R SQL-related packages
