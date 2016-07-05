77eeb830b6b06b1840f2df602270aee4e705510a
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-07-05 16:53:43 -0700

    fix installation of tensor flow

--- Python/Install-DeepLearning-Packages.sh
+++ Python/Install-DeepLearning-Packages.sh
@@ -34,7 +34,7 @@ conda update wheel
 
 conda update numpy
 
-pip install --no-dependencies https://storage.googleapis.com/tensorflow/mac/tensorflow-0.9.0-py2-none-any.whl
+pip install https://storage.googleapis.com/tensorflow/mac/tensorflow-0.9.0-py2-none-any.whl
 
 
 # GNumPy
