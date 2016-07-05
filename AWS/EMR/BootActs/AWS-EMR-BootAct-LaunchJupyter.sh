a9e8b0ca4f665a03746143d3f0c413eaa025f705
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-07-04 18:12:45 -0700

    update Jupyter launch script

--- AWS/EMR/BootActs/AWS-EMR-BootAct-LaunchJupyter.sh
+++ AWS/EMR/BootActs/AWS-EMR-BootAct-LaunchJupyter.sh
@@ -17,6 +17,22 @@ then
     # change to apps directory
     cd $APPS_DIR
 
+    # install ZeroMQ
+    git clone https://github.com/zeromq/libzmq
+    cd libzmq
+    # wget https://github.com/zeromq/zeromq4-1/releases/download/v4.1.5/zeromq-4.1.5.tar.gz
+    # tar xzf zeromq-*.tar.gz
+    # sudo rm zeromq-*.tar.gz
+    # cd zeromq-*
+    ./autogen.sh
+    ./configure --prefix=$ZMQ_DIR
+    make
+    sudo make install
+    sudo ldconfig
+    cd ..
+    sudo rm -r libzmq
+    # sudo rm -r zeromq-*
+    echo `df -h / | sed -n 2p` ZeroMQ >> $MAIN_DISK_USAGE_LOG
 
     # install iR kernel
     wget $GITHUB_REPO_RAW_PATH/BootActs/AWS-EMR-BootAct-InstallJupyterIRKernelRelatedPackages.R
@@ -35,22 +51,17 @@ then
     # install iJulia kernel
     wget $GITHUB_REPO_RAW_PATH/BootActs/AWS-EMR-BootAct-InstallJupyterIJuliaKernel.jl
     dos2unix AWS-EMR-BootAct-InstallJupyterIJuliaKernel.jl
-    set +e
     julia AWS-EMR-BootAct-InstallJupyterIJuliaKernel.jl
-    set -e
     sudo rm AWS-EMR-BootAct-InstallJupyterIJuliaKernel.jl
 
     echo `df -h / | sed -n 2p` Jupyter iJulia Kernel >> $MAIN_DISK_USAGE_LOG
 
 
     # install Scala kernel
-    wget https://oss.sonatype.org/content/repositories/snapshots/com/github/alexarchambault/jupyter/jupyter-scala-cli_2.11.6/0.2.0-SNAPSHOT/jupyter-scala_2.11.6-0.2.0-SNAPSHOT.zip
-    unzip jupyter-scala_*.zip
-    sudo rm jupyter-scala_*.zip
-    cd jupyter-scala_*/bin
-    chmod 777 jupyter-scala
+    curl -L -o jupyter-scala https://git.io/vrHhi
     ./jupyter-scala
-    sudo mv /home/hadoop/.ipython/kernels/scala211/ ~/.local/share/jupyter/kernels/
+    sudo rm jupyter-scala
+    sudo mv /home/hadoop/.local/share/jupyter/kernels/scala211/ ~/.local/share/jupyter/kernels/
 
     echo `df -h / | sed -n 2p` Jupyter Scala Kernel >> $MAIN_DISK_USAGE_LOG
 
