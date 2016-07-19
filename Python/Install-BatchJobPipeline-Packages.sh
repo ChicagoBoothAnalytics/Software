fc8c144eebe5fb2a598b0e8273b3ed42b9057a55
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-07-19 15:04:51 -0700

    update installations of Jupyter packages & Batch processing packages

---
+++ Python/Install-BatchJobPipeline-Packages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# this script requires an existing installation of Anaconda Python
+
+
+# Luigi
+conda install Luigi
+conda update luigi
--- Python/Install-Jupyter-Packages.sh
+++ Python/Install-Jupyter-Packages.sh
@@ -3,6 +3,12 @@
 # this script requires an existing installation of Anaconda Python
 
 
+# Jupyter NbExtensions Configurator
+pip install --upgrade Jupyter_NbExtensions_Configurator
+
+# iPython Notebook Extensions
+pip install https://github.com/ipython-contrib/IPython-notebook-extensions/tarball/master
+
 # RunIPy
 conda install RunIPy
 conda update runipy
