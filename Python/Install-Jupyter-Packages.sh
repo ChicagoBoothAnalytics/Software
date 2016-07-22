34c83a6ed203dab844cbd3cf42283ce2c9717877
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-07-21 20:51:47 -0700

    update installation of Jupyter packages

--- Python/Install-Jupyter-Packages.sh
+++ Python/Install-Jupyter-Packages.sh
@@ -3,11 +3,18 @@
 # this script requires an existing installation of Anaconda Python
 
 
+# update iPython / Jupyter
+conda update ipython
+conda update jupyter
+
 # Jupyter NbExtensions Configurator
 pip install --upgrade Jupyter_NbExtensions_Configurator
 
 # iPython Notebook Extensions
 pip install https://github.com/ipython-contrib/IPython-notebook-extensions/tarball/master
+
+# update NBConvert
+conda update nbconvert
 
 # RunIPy
 conda install RunIPy
