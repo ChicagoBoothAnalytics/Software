76a1ef4852594eb95aa5c870c01050e5ca57f503
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-07-22 13:27:33 -0700

    add installation of AWS Boto

--- Python/Install-AWS-Packages.sh
+++ Python/Install-AWS-Packages.sh
@@ -3,4 +3,9 @@
 # this script requires an existing installation of Anaconda Python
 
 
+# AWS Command-Line Interface
 pip install --upgrade AWSCLI
+
+# Boto
+pip install --upgrade Boto
+pip install --upgrade Boto3
