66273e5b7a7aa1073e4e3f2847b4c64d8e492d80
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2016-08-24 00:12:55 -0700

    update AWS EMR create0.sh

--- AWS/EMR/create0.sh
+++ AWS/EMR/create0.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 
 # parse command-line options
-while getopts "m:p:n:t:q:r:" opt
+while getopts "m:p:n:t:q:a:r:k:z:" opt
 do
     case $opt in
         m)
@@ -20,15 +20,20 @@ do
         q)
             WORKER_INSTANCE_PRICE=$OPTARG
             ;;
+        a)
+            AWS_PROFILE=$OPTARG
+            ;;
         r)
+            AWS_REGION=$OPTARG
+            ;;
+        k)
+            AWS_KEY_PAIR=$OPTARG
+            ;;
+        z)
             REMARKS=$OPTARG
             ;;
     esac
 done
-
-
-# CONSTANTS
-AWS_REGION=us-west-1
 
 
 echo "Bidding for bare AWS EMR cluster with 1 x $MASTER_INSTANCE_TYPE Master @ \$$MASTER_INSTANCE_PRICE/node/hr + $NB_WORKER_NODES x $WORKER_INSTANCE_TYPE Core Workers @ \$$WORKER_INSTANCE_PRICE/node/hr..."
@@ -37,17 +42,19 @@ aws emr create-cluster \
     --name \
         "$REMARKS (bare): 1 x $MASTER_INSTANCE_TYPE Master @ \$$MASTER_INSTANCE_PRICE/node/hr + $NB_WORKER_NODES x $WORKER_INSTANCE_TYPE Core Workers @ \$$WORKER_INSTANCE_PRICE/node/hr" \
     --release-label \
-        emr-4.7.2 \
+        emr-5.0.0 \
     --instance-groups \
         InstanceGroupType=MASTER,InstanceCount=1,InstanceType=$MASTER_INSTANCE_TYPE,BidPrice=$MASTER_INSTANCE_PRICE \
         InstanceGroupType=CORE,InstanceCount=$NB_WORKER_NODES,InstanceType=$WORKER_INSTANCE_TYPE,BidPrice=$WORKER_INSTANCE_PRICE \
     --no-auto-terminate \
     --use-default-roles \
     --ec2-attributes \
-        KeyName=keypair \
+        KeyName=$AWS_KEY_PAIR \
     --no-termination-protected \
     --visible-to-all-users \
     --applications \
-        Name=Spark
+        Name=Spark \
+    --profile \
+        $AWS_PROFILE
 
 echo "For your cluster's status, please check: $AWS_REGION.console.aws.amazon.com/elasticmapreduce"
